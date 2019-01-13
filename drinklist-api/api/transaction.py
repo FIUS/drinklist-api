cde65a2a973b82c2c5aa9e320b1f2dcbaad92f90
Markus Z <st114857@stud.uni.stuttgart.de>
2019-01-13 06:08:55 +0100

    half way to stock control

--- drinklist-api/api/transaction.py
+++ drinklist-api/api/transaction.py
@@ -79,6 +79,7 @@ class TransactionList(Resource):
                     abort(400, 'Specified beverage does not exist')
                 new_beverage = TransactionBeverage(new_transaction, refered_beverage, beverage['count'], refered_beverage.price)
                 new_amount += beverage['count']*refered_beverage.price
+                refered_beverage.stock += beverage['count']
                 DB.session.add(new_beverage)
             if not (beverages is None or len(beverages) == 0):
                 new_transaction.amount = new_amount
@@ -147,6 +148,8 @@ class UserDetail(Resource):
                     reversed_beverage = TransactionBeverage(reverse_transaction, beverage.beverage, -(beverage.count), beverage.price)
                     DB.session.add(reversed_beverage)
                     new_amount += reversed_beverage.count*reversed_beverage.price
+                    refered_beverage = Beverage.query.filter(Beverage.id == reversed_beverage.beverage_id).first()
+                    refered_beverage.stock -= reversed_beverage.count
                 reverse_transaction.amount = new_amount
                 user.balance += new_amount
                 DB.session.commit()
