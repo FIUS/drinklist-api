1dc054743684373dea8d9b1e30ce8f97e33cd7b4
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2019-01-13 06:10:35 +0100

    Add advanced logging configs.

--- drinklist-api/config.py
+++ drinklist-api/config.py
@@ -16,6 +16,11 @@ class Config(object):
 
     LOGIN_PROVIDERS = ['Basic']
 
+    BASIC_AUTH_USERS = {}
+    BASIC_AUTH_ADMIN = []
+    BASIC_AUTH_KIOSK = []
+    BASIC_AUTH_USER = []
+
     LDAP_URI = ""
     LDAP_PORT = 0
     LDAP_SSL = False
@@ -31,6 +36,68 @@ class Config(object):
     LDAP_CONSUMER_GROUP_FILTER = ""
     LDAP_KIOSK_USER_GROUP_FILTER = ""
     LDAP_ADMIN_GROUP_FILTER = ""
+
+    LOGGING = {
+        'version': 1,
+        'formatters': {
+            'extended': {
+                'format': '%(asctime)s [%(levelname)s] [%(name)-16s] %(message)s <%(module)s, \
+                %(funcName)s, %(lineno)s; %(pathname)s>',
+            },
+            'short': {
+                'format': '[%(asctime)s] [%(levelname)s] [%(name)-16s] %(message)s',
+            }
+        },
+        'handlers': {
+            'default': {
+                'class': 'logging.handlers.RotatingFileHandler',
+                'formatter': 'extended',
+                'filename': '/tmp/drinklist-api-default.log',
+                'maxBytes': 104857600,
+                'backupCount': 10,
+            },
+            'auth': {
+                'class': 'logging.handlers.RotatingFileHandler',
+                'formatter': 'short',
+                'filename': '/tmp/drinklist-api-auth.log',
+                'maxBytes': 104857600,
+                'backupCount': 10,
+            },
+            'query': {
+                'class': 'logging.handlers.RotatingFileHandler',
+                'formatter': 'short',
+                'filename': '/tmp/drinklist-api-querys.log',
+                'maxBytes': 104857600,
+                'backupCount': 2,
+            },
+            'console': {
+                'class' : 'logging.StreamHandler',
+                'formatter': 'extended',
+            }
+        },
+        'loggers': {
+            'flask.app.auth': {
+                'level': logging.INFO,
+                'propagate': False,
+                'handlers': ['auth'],
+            },
+            'flask.app.db': {
+                'level': logging.WARNING,
+                'propagate': False,
+                'handlers': ['query'],
+            },
+            'sqlalchemy': {
+                'level': logging.WARNING,
+                'propagate': False,
+                'handlers': ['query'],
+            },
+        },
+        'root': {
+            'level': logging.WARNING,
+            'handlers': ['default'],
+        },
+        'disable_existing_loggers': True,
+    }
 
     JSON_SORT_KEYS = False
     JSONIFY_PRETTYPRINT_REGULAR = False
@@ -55,6 +122,15 @@ class DebugConfig(Config):
     BASIC_AUTH_ADMIN = ["admin"]
     BASIC_AUTH_KIOSK = ["kiosk"]
     BASIC_AUTH_USER = ["user1", "user2"]
+
+    Config.LOGGING['loggers']['flask.app.auth']['level'] = logging.DEBUG
+    Config.LOGGING['loggers']['flask.app.db']['level'] = logging.DEBUG
+    Config.LOGGING['loggers']['sqlalchemy.engine'] = {
+        'level': logging.WARN,
+        'propagate': False,
+        'handlers': ['query'],
+    }
+    Config.LOGGING['root']['handlers'].append('console')
 
 class TestingConfig(Config):
     TESTING = True
